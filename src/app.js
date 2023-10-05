894b093a8819c96f38f2cc4416f9df889863182a
vintech05 <kotakkelpo@gmail.com>
2023-10-05 13:20:38 +0700

    added search bar functionality

--- src/app.js
+++ src/app.js
@@ -26,4 +26,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
         form.classList.remove('active');
     })
 
+    function searchTable() {
+        var input, filter, table, tr, td, i, j;
+        input = document.getElementById("search-bar");
+        filter = input.value.toUpperCase();
+        table = document.querySelector(".interns-table");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td");
+            for (j = 0; j < td.length; j++) {
+                if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    break;
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    } 
+
+    document.querySelector('#search-bar').addEventListener('keyup', searchTable);
+
 });
